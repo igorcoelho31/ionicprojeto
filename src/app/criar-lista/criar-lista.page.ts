f5809c267de6199b2d8f11151b280ebb09282fe1
igorcoelho31 <113933078+igorcoelho31@users.noreply.github.com>
2023-08-29 11:01:42 -0300

    Add files via upload

---
+++ src/app/criar-lista/criar-lista.page.ts
@@ -0,0 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Lista } from '../model/lista.model';
+
+@Component({
+  selector: 'app-criar-lista',
+  templateUrl: './criar-lista.page.html',
+  styleUrls: ['./criar-lista.page.scss'],
+})
+
+export class CriarListaPage{
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type' : 'application/json'})
+  };
+
+  
+
+  meusProdutos:Lista[] = [];
+
+  nameButton = "Adicionar";
+  dados = {
+    id: "",
+    produto: "",
+    quantidade: ""
+};
+
+  constructor(
+    private http:HttpClient,
+  ) {}
+  ngOnInit(): void {
+
+    this.http.get<Lista[]>('http://localhost:3000/lista').subscribe(results => this.meusProdutos = results );
+  }
+
+  cadastro(form:any){
+    this.http.post('http://localhost:3000/lista/', form.value, this.httpOptions).subscribe();
+    setTimeout(this.refresh, 1000) 
+  }
+
+
+  deletar(id:any){
+    this.http.delete('http://localhost:3000/lista/' + id).subscribe();
+    // Definir 2 segundos para atualizar a página
+    setTimeout(this.refresh, 1000) 
+  }
+
+  // Método de refresh
+
+refresh(){
+  // Função JS - Atualiza a página
+  location.reload();
+}
+
+}
---
+++ src/app/model/lista.model.ts
@@ -0,0 +1,6 @@
+export interface Lista{
+    id: number,
+    quantidade: number,
+    produto: string;
+
+}
